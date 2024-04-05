fcf653fdbbff5914386412ba8340eb62e87cfaa3
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-05 14:18:40 -0300

    feat: basic main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("alo");
+import csv from "csvtojson";
+import StockTaxCalculator from "./StockTaxCalculator.js";
+import Transaction from "./Transaction.js";
+import mapCsvTransactionToTransaction, { CsvTransaction } from "./TransactionMapper.js";
+
+const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
+const transactions: Transaction[] = csvTransactions.map(mapCsvTransactionToTransaction);
+
+const stockTax = new StockTaxCalculator(transactions);
+console.log(stockTax.getSituation(2021));
