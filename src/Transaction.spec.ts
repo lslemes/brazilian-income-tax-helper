beee7bdd64e38b5efb689804eec5c7b79b5ef3ec
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-05 19:28:34 -0300

    test: transaction value

--- src/Transaction.spec.ts
+++ src/Transaction.spec.ts
@@ -2,9 +2,15 @@
 import Transaction from "./Transaction";
 
 describe("Transaction", () => {
-	it("should correctly calculate transaction value", () => {
+	it.each([
+		[12, 19.45, 12 * 19.45],
+		[0, 10, 0],
+		[10, 0, 0],
+		[0, 0, 0],
+		[6486, 13.14, 6486 * 13.14],
+	])("should correctly calculate transaction value", (quantity, price, value) => {
 		const anything: any = undefined;
-		const transaction = new Transaction(anything, anything, anything, 263, 64.17);
-		expect(transaction.value).toBe(263 * 64.17);
+		const transaction = new Transaction(anything, anything, anything, quantity, price);
+		expect(transaction.value).toBe(value);
 	});
 });
