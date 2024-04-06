197227b77da75008a0b33962fa9bc5fe549a0c4e
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-06 13:35:20 -0300

    test: getYearlyTaxData

--- src/TaxCalculator.spec.ts
+++ src/TaxCalculator.spec.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import csv from "csvtojson";
 import TaxCalculator from "./TaxCalculator";
+import Transaction, { TransactionType } from "./Transaction";
+import mapCsvTransactionToTransaction, { CsvTransaction } from "./TransactionMapper";
 
 describe("TaxCalculator", () => {
 	describe("getUpdatedAveragePrice", () => {
@@ -63,5 +66,187 @@ describe("TaxCalculator", () => {
 		[0.991, 0.99],
 	])("getMonetaryValue(%p) = %p", (value, monetaryValue) => {
 		expect(TaxCalculator["getMonetaryValue"](value)).toBe(monetaryValue);
+	});
+
+	describe("getMonthlyProfit", () => {});
+
+	describe("getSituation", () => {});
+
+	describe("getYearlyTaxData", () => {
+		let transactions: Transaction[];
+
+		beforeAll(async () => {
+			const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
+			transactions = csvTransactions.map(mapCsvTransactionToTransaction);
+		});
+
+		it("should return buy transactions with no profits", async () => {
+			const { transactionsWithProfit } = TaxCalculator["getYearlyTaxData"](transactions);
+			const buyTransactions = transactionsWithProfit.filter((transaction) => transaction.type === TransactionType.Buy);
+			for (const transaction of buyTransactions) expect(transaction.profit).toBeNull();
+		});
+
+		it("should return sell transactions with profits", async () => {
+			const { transactionsWithProfit } = TaxCalculator["getYearlyTaxData"](transactions);
+			const sellTransactions = transactionsWithProfit.filter(
+				(transaction) => transaction.type === TransactionType.Sell,
+			);
+			for (const transaction of sellTransactions) expect(transaction.profit).toEqual(expect.any(Number));
+		});
+
+		it("should return a yearly position map", () => {
+			const { positionMapByYear } = TaxCalculator["getYearlyTaxData"](transactions);
+			expect(positionMapByYear.get(2019)).toStrictEqual(
+				new Map([
+					["ABEV3", 37],
+					["ALZR11", 7],
+					["B3SA3", 13],
+					["BIDI3", 10],
+					["CIEL3", 22],
+					["EGIE3", 14],
+					["FLRY3", 24],
+					["GGRC11", 6],
+					["GRND3", 59],
+					["HGBS11", 3],
+					["HGLG11", 5],
+					["HGRE11", 5],
+					["HYPE3", 21],
+					["IRBR3", 18],
+					["ITUB3", 22],
+					["KNRI11", 5],
+					["LREN3", 13],
+					["MDIA3", 100],
+					["MULT3", 22],
+					["ODPV3", 44],
+					["PSSA3", 11],
+					["RADL3", 9],
+					["VISC11", 7],
+					["VRTA11", 7],
+					["WEGE3", 21],
+					["XPLG11", 7],
+					["XPML11", 7],
+				]),
+			);
+			expect(positionMapByYear.get(2020)).toStrictEqual(
+				new Map([
+					["ABEV3", 55],
+					["ALZR11", 11],
+					["CIEL3", 22],
+					["EGIE3", 19],
+					["FLMA11", 82],
+					["FLRY3", 33],
+					["GGRC11", 12],
+					["GRND3", 87],
+					["HGBS11", 7],
+					["HGLG11", 18],
+					["HGRE11", 8],
+					["IRBR3", 25],
+					["KNRI11", 19],
+					["LREN3", 19],
+					["MULT3", 31],
+					["ODPV3", 61],
+					["VISC11", 11],
+					["VRTA11", 11],
+					["XPLG11", 22],
+					["XPML11", 29],
+				]),
+			);
+			expect(positionMapByYear.get(2021)).toStrictEqual(new Map());
+			expect(positionMapByYear.get(2022)).toStrictEqual(new Map());
+			expect(positionMapByYear.get(2023)).toStrictEqual(
+				new Map([
+					["BOVA11", 10],
+					["IMAB11", 66],
+					["IRFM11", 27],
+					["XFIX11", 34],
+				]),
+			);
+			expect(positionMapByYear.get(2024)).toStrictEqual(
+				new Map([
+					["B5P211", 146],
+					["BOVA11", 10],
+					["IRFM11", 27],
+					["IVVB11", 47],
+				]),
+			);
+		});
+
+		it("should return a yearly average price map", () => {
+			const { averagePriceMapByYear } = TaxCalculator["getYearlyTaxData"](transactions);
+			console.log("averagePriceMapByYear :>> ", averagePriceMapByYear);
+			expect(averagePriceMapByYear.get(2019)).toStrictEqual(
+				new Map([
+					["ABEV3", expect.closeTo(677.47 / 37, 12)],
+					["ALZR11", expect.closeTo(850.08 / 7, 12)],
+					["B3SA3", expect.closeTo(445.79 / 13, 12)],
+					["BIDI3", expect.closeTo(155.41 / 10, 12)],
+					["CIEL3", expect.closeTo(178.16 / 22, 12)],
+					["EGIE3", expect.closeTo(666.37 / 14, 12)],
+					["FLRY3", expect.closeTo(691.44 / 24, 12)],
+					["GGRC11", expect.closeTo(853.08 / 6, 12)],
+					["GRND3", expect.closeTo(685.58 / 59, 12)],
+					["HGBS11", expect.closeTo(855.6 / 3, 12)],
+					["HGLG11", expect.closeTo(933.03 / 5, 12)],
+					["HGRE11", expect.closeTo(839.5 / 5, 12)],
+					["HYPE3", expect.closeTo(675.93 / 21, 12)],
+					["IRBR3", expect.closeTo(667.26 / 18, 12)],
+					["ITUB3", expect.closeTo(683.76 / 22, 12)],
+					["KNRI11", expect.closeTo(899.95 / 5, 12)],
+					["LREN3", expect.closeTo(693.68 / 13, 12)],
+					["MDIA3", expect.closeTo(3947.97 / 100, 12)],
+					["MULT3", expect.closeTo(667.26 / 22, 12)],
+					["ODPV3", expect.closeTo(694.76 / 44, 12)],
+					["PSSA3", expect.closeTo(661.32 / 11, 12)],
+					["RADL3", expect.closeTo(950.37 / 9, 12)],
+					["VISC11", expect.closeTo(942.75 / 7, 12)],
+					["VRTA11", expect.closeTo(904.19 / 7, 12)],
+					["WEGE3", expect.closeTo(686.7 / 21, 12)],
+					["XPLG11", expect.closeTo(923.09 / 7, 12)],
+					["XPML11", expect.closeTo(890.72 / 7, 12)],
+				]),
+			);
+			expect(averagePriceMapByYear.get(2020)).toStrictEqual(
+				new Map([
+					["ABEV3", expect.closeTo(1001.91 / 55, 12)],
+					["ALZR11", expect.closeTo(1435.52 / 11, 12)],
+					["CIEL3", expect.closeTo(178.16 / 22, 12)],
+					["EGIE3", expect.closeTo(927.07 / 19, 12)],
+					["FLMA11", expect.closeTo(250.9 / 82, 12)],
+					["FLRY3", expect.closeTo(966.48 / 33, 12)],
+					["GGRC11", expect.closeTo(1751.18 / 12, 12)],
+					["GRND3", expect.closeTo(1026.57 / 87, 12)],
+					["HGBS11", expect.closeTo(2055.37 / 7, 12)],
+					["HGLG11", expect.closeTo(3483.29 / 18, 12)],
+					["HGRE11", expect.closeTo(1481.02 / 8, 12)],
+					["IRBR3", expect.closeTo(940.61 / 25, 12)],
+					["KNRI11", expect.closeTo(3355.47 / 19, 12)],
+					["LREN3", expect.closeTo(1019.67 / 19, 12)],
+					["MULT3", expect.closeTo(968.76 / 31, 12)],
+					["ODPV3", expect.closeTo(983.93 / 61, 12)],
+					["VISC11", expect.closeTo(1522.23 / 11, 12)],
+					["VRTA11", expect.closeTo(1484.03 / 11, 12)],
+					["XPLG11", expect.closeTo(2941.24 / 22, 12)],
+					["XPML11", expect.closeTo(3340.34 / 29, 12)],
+				]),
+			);
+			expect(averagePriceMapByYear.get(2021)).toStrictEqual(new Map());
+			expect(averagePriceMapByYear.get(2022)).toStrictEqual(new Map());
+			expect(averagePriceMapByYear.get(2023)).toStrictEqual(
+				new Map([
+					["BOVA11", expect.closeTo(976.7 / 10, 12)],
+					["IMAB11", expect.closeTo(5936.69 / 66, 12)],
+					["IRFM11", expect.closeTo(1969.11 / 27, 12)],
+					["XFIX11", expect.closeTo(336.6 / 34, 12)],
+				]),
+			);
+			expect(averagePriceMapByYear.get(2024)).toStrictEqual(
+				new Map([
+					["B5P211", expect.closeTo((41 * 87.78 + 105 * 88) / (41 + 105), 12)],
+					["BOVA11", expect.closeTo(97.67, 12)],
+					["IRFM11", expect.closeTo(72.93, 12)],
+					["IVVB11", expect.closeTo((12 * 289.4 + 35 * 288.74) / (12 + 35), 12)],
+				]),
+			);
+		});
 	});
 });
--- src/TaxCalculator.ts
+++ src/TaxCalculator.ts
@@ -2,44 +2,16 @@ import Transaction, { TransactionType } from "./Transaction";
 import { MONTHS } from "./utils";
 
 export default abstract class TaxCalculator {
-	private readonly positionMaps = new Map<number, Map<string, number>>();
-	private readonly averagePriceMaps = new Map<number, Map<string, number>>();
+	private readonly positionMapByYear = new Map<number, Map<string, number>>();
+	private readonly averagePriceMapByYear = new Map<number, Map<string, number>>();
+	protected readonly transactions: Transaction[];
 
-	constructor(protected readonly transactions: Transaction[]) {
-		const positionMap = new Map<string, number>();
-		const averagePriceMap = new Map<string, number>();
-
-		let currentYear: number | null = null;
-		for (const transaction of this.transactions) {
-			const year = transaction.date.getFullYear();
-			if (currentYear === null) currentYear = year;
-			if (year !== currentYear) {
-				this.positionMaps.set(currentYear, new Map(positionMap));
-				this.averagePriceMaps.set(currentYear, new Map(averagePriceMap));
-				currentYear = year;
-			}
-
-			const { quantity, value, type } = transaction;
-			const ticker = transaction.asset.code;
-			const position = positionMap.get(ticker) ?? 0;
-			const averagePrice = averagePriceMap.get(ticker) ?? 0;
-
-			switch (type) {
-				case TransactionType.Buy:
-					positionMap.set(ticker, position + quantity);
-					averagePriceMap.set(ticker, TaxCalculator.getUpdatedAveragePrice(position, averagePrice, quantity, value));
-					break;
-				case TransactionType.Sell:
-					transaction.profit = value - quantity * averagePrice;
-					if (position - quantity <= 0) averagePriceMap.delete(ticker);
-					positionMap.set(ticker, position - quantity);
-					break;
-			}
-		}
-		if (currentYear !== null) {
-			this.positionMaps.set(currentYear, new Map(positionMap));
-			this.averagePriceMaps.set(currentYear, new Map(averagePriceMap));
-		}
+	constructor(transactions: Transaction[]) {
+		const { averagePriceMapByYear, positionMapByYear, transactionsWithProfit } =
+			TaxCalculator.getYearlyTaxData(transactions);
+		this.transactions = transactionsWithProfit;
+		this.averagePriceMapByYear = averagePriceMapByYear;
+		this.positionMapByYear = positionMapByYear;
 	}
 
 	private static getUpdatedAveragePrice(
@@ -59,6 +31,56 @@ export default abstract class TaxCalculator {
 		return Number(value.toFixed(2));
 	}
 
+	private static getYearlyTaxData(transactions: Transaction[]) {
+		const positionByAssetCode = new Map<string, number>();
+		const averagePriceByAssetCode = new Map<string, number>();
+		const positionMapByYear = new Map<number, typeof positionByAssetCode>();
+		const averagePriceMapByYear = new Map<number, typeof averagePriceByAssetCode>();
+
+		let currentYear: number | null = null;
+		const transactionsWithProfit = transactions.map((transaction) => {
+			const year = transaction.date.getFullYear();
+			if (currentYear === null) currentYear = year;
+			while (year > currentYear) {
+				positionMapByYear.set(currentYear, new Map(positionByAssetCode));
+				averagePriceMapByYear.set(currentYear, new Map(averagePriceByAssetCode));
+				currentYear++;
+			}
+
+			const { quantity, value, type } = transaction;
+			const assetCode = transaction.asset.code;
+			const position = positionByAssetCode.get(assetCode) ?? 0;
+			const averagePrice = averagePriceByAssetCode.get(assetCode) ?? 0;
+
+			switch (type) {
+				case TransactionType.Buy:
+					positionByAssetCode.set(assetCode, position + quantity);
+					averagePriceByAssetCode.set(
+						assetCode,
+						TaxCalculator.getUpdatedAveragePrice(position, averagePrice, quantity, value),
+					);
+					break;
+				case TransactionType.Sell:
+					transaction.profit = value - quantity * averagePrice;
+					if (position - quantity <= 0) {
+						averagePriceByAssetCode.delete(assetCode);
+						positionByAssetCode.delete(assetCode);
+					} else {
+						positionByAssetCode.set(assetCode, position - quantity);
+					}
+					break;
+			}
+
+			return transaction;
+		});
+		if (currentYear !== null) {
+			positionMapByYear.set(currentYear, new Map(positionByAssetCode));
+			averagePriceMapByYear.set(currentYear, new Map(averagePriceByAssetCode));
+		}
+
+		return { transactionsWithProfit, positionMapByYear, averagePriceMapByYear };
+	}
+
 	getMonthlyProfit() {
 		return MONTHS.map((month) => ({
 			month: month.label,
@@ -72,9 +94,9 @@ export default abstract class TaxCalculator {
 
 	getSituation(year: number) {
 		const situationMap = new Map<string, { position: number; situation: number }>();
-		for (const [assetCode, position] of this.positionMaps.get(year)!) {
+		for (const [assetCode, position] of this.positionMapByYear.get(year)!) {
 			if (position < 0) throw new Error(`Negative position ${position} ${assetCode}.`);
-			const averagePriceMap = this.averagePriceMaps.get(year)!;
+			const averagePriceMap = this.averagePriceMapByYear.get(year)!;
 			situationMap.set(assetCode, { position: position, situation: position * averagePriceMap.get(assetCode)! });
 		}
 		const sortedSituationMap = new Map([...situationMap].sort());
