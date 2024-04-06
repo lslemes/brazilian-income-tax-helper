de3a999b11ba4d251d0ad1bedf3493f94d130fb1
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-06 20:33:33 -0300

    feat: subscription tax report

--- src/taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator.ts
+++ src/taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator.ts
@@ -2,7 +2,13 @@ import Transaction, { AssetType } from "../../transaction/Transaction";
 import TaxCalculator from "../TaxCalculator";
 
 export default class SubscriptionTaxCalculator extends TaxCalculator {
+	private static readonly darfRate = 0.15;
+
 	constructor(transactions: Transaction[]) {
 		super(transactions.filter((transaction) => transaction.asset.type === AssetType.Subscription));
+	}
+
+	public getTaxReport(year: number) {
+		return super.getTaxReport(year, SubscriptionTaxCalculator.darfRate);
 	}
 }
