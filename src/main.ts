c745c91e0b82742d76d7768f37a67978f6d49565
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-06 15:43:42 -0300

    chore: update main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import csv from "csvtojson";
-import StockTaxCalculator from "./StockTaxCalculator.js";
 import Transaction from "./Transaction.js";
 import mapCsvTransactionToTransaction, { CsvTransaction } from "./TransactionMapper.js";
 
 async function main() {
 	const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
 	const transactions: Transaction[] = csvTransactions.map(mapCsvTransactionToTransaction);
-
-	const stockTax = new StockTaxCalculator(transactions);
-	console.log(stockTax.getSituation(2019));
+	console.log("transactions :>> ", transactions);
 }
 main();
