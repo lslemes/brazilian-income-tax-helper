b4b3f3dbde16badc5412404dbf49d3c12f9f0db7
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-06 20:33:28 -0300

    feat: fii tax report

--- src/taxCalculators/fiiTaxCalculator/FiiTaxCalculator.ts
+++ src/taxCalculators/fiiTaxCalculator/FiiTaxCalculator.ts
@@ -2,7 +2,13 @@ import Transaction, { AssetType } from "../../transaction/Transaction";
 import TaxCalculator from "../TaxCalculator";
 
 export default class FiiTaxCalculator extends TaxCalculator {
+	private static readonly darfRate = 0.2;
+
 	constructor(transactions: Transaction[]) {
 		super(transactions.filter((transaction) => transaction.asset.type === AssetType.Fii));
+	}
+
+	public getTaxReport(year: number) {
+		return super.getTaxReport(year, FiiTaxCalculator.darfRate);
 	}
 }
