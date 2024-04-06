b9467c1816a0d1c8d81b9cb1bb588da16c485276
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-06 18:02:13 -0300

    refactor: variable name

--- src/FiiTaxCalculator.ts
+++ src/FiiTaxCalculator.ts
@@ -3,6 +3,6 @@ import Transaction, { AssetType } from "./Transaction";
 
 export default class FiiTaxCalculator extends TaxCalculator {
 	constructor(transactions: Transaction[]) {
-		super(transactions.filter((t) => t.asset.type === AssetType.Fii));
+		super(transactions.filter((transaction) => transaction.asset.type === AssetType.Fii));
 	}
 }
