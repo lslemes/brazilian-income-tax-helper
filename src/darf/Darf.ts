b4b5aa694ef796df8dfc2622a9ff82ec1e513f3a
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-08 13:23:32 -0300

    chore: darf todo

--- src/darf/Darf.ts
+++ src/darf/Darf.ts
@@ -3,7 +3,7 @@ import { MonthLabel } from "../utils";
 export default class Darf {
 	constructor(
 		private readonly year: number,
-		private readonly month: MonthLabel,
+		private readonly month: MonthLabel, // mudar para receber um Month
 		private readonly value: number,
 	) {}
 }
