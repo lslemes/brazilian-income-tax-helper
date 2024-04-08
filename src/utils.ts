7b10417961ef8df523396b2b402cc4f84b42eb8d
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-08 13:23:24 -0300

    feat: month label type

--- src/utils.ts
+++ src/utils.ts
@@ -14,5 +14,6 @@ export const MONTHS = [
 ] as const;
 
 export type MonthLabel = (typeof MONTHS)[number]["label"];
+export type MonthValue = (typeof MONTHS)[number]["value"];
 
 export const FLOATING_POINT_PRECISION = 11 as const;
