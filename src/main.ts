de00d9a6992df2336a6ec6112ccac21e9fb1c25e
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-08 13:24:21 -0300

    chore: sample main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import csv from "csvtojson";
+import EtfTaxCalculator from "./taxCalculators/etfTaxCalculator/EtfTaxCalculator";
+import FiiTaxCalculator from "./taxCalculators/fiiTaxCalculator/FiiTaxCalculator";
+import StockTaxCalculator from "./taxCalculators/stockTaxCalculator/StockTaxCalculator";
+import SubscriptionTaxCalculator from "./taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator";
 import Transaction from "./transaction/Transaction";
 import mapCsvTransactionToTransaction, { CsvTransaction } from "./transaction/mapCsvTransactionToTransaction";
 
 async function main() {
 	const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
 	const transactions: Transaction[] = csvTransactions.map(mapCsvTransactionToTransaction);
-	console.log("transactions :>> ", transactions);
+
+	const fiiTaxCalculator = new FiiTaxCalculator(transactions);
+	const stockTaxCalculator = new StockTaxCalculator(transactions);
+	const etfTaxCalculator = new EtfTaxCalculator(transactions);
+	const subscriptionTaxCalculator = new SubscriptionTaxCalculator(transactions);
+	// console.log(stockTaxCalculator.getTaxReport(2024));
 }
 main();
