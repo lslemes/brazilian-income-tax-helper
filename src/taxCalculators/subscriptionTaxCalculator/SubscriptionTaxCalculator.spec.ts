12b6def67f77f02c03fe280f0c21000aefa9ed8e
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-08 20:33:23 -0300

    test: subscription tax calculator

--- src/taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator.spec.ts
+++ src/taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator.spec.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import csv from "csvtojson";
 import Darf from "../../darf/Darf";
-import { getExpectedTaxReport } from "../../testUtils";
 import { AssetType } from "../../transaction/Transaction";
 import mapCsvTransactionToTransaction, { CsvTransaction } from "../../transaction/mapCsvTransactionToTransaction";
 import { MONTHS } from "../../utils";
@@ -13,6 +13,10 @@ describe("SubscriptionTaxCalculator", () => {
 		const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
 		const transactions = csvTransactions.map(mapCsvTransactionToTransaction);
 		subscriptionTaxCalculator = new SubscriptionTaxCalculator(transactions);
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
 	});
 
 	it("should filter subscription transactions", () => {
@@ -49,21 +53,40 @@ describe("SubscriptionTaxCalculator", () => {
 		[2022, new Map()],
 		[2023, new Map()],
 		[2024, new Map()],
-	])("getSituationReport(%p)", (year, expectedReport) => {
-		expect(subscriptionTaxCalculator["getSituationReport"](year)).toStrictEqual(expectedReport);
+	])("getSituationReport(%p)", (year, expectedSituationReport) => {
+		expect(subscriptionTaxCalculator["getSituationReport"](year)).toStrictEqual(expectedSituationReport);
 	});
 
 	test.each([
-		[2019, getExpectedTaxReport(new Map(), new Array(12).fill(0), [])],
-		[
-			2020,
-			getExpectedTaxReport(new Map(), [0, 0, 0, 0, 0, 0, 9.28, 0, 0, 0, 0, 0], [new Darf(2020, MONTHS[6].label, 1.39)]),
-		],
-		[2021, getExpectedTaxReport(new Map(), new Array(12).fill(0), [])],
-		[2022, getExpectedTaxReport(new Map(), new Array(12).fill(0), [])],
-		[2023, getExpectedTaxReport(new Map(), new Array(12).fill(0), [])],
-		[2024, getExpectedTaxReport(new Map(), new Array(12).fill(0), [])],
-	])("getTaxReport(%p)", (year, expectedTaxReport) => {
-		expect(subscriptionTaxCalculator["getTaxReport"](year)).toStrictEqual(expectedTaxReport);
+		[2019, []],
+		[2020, [new Darf(2020, MONTHS[6].label, 1.39)]],
+		[2021, []],
+		[2022, []],
+		[2023, []],
+		[2024, []],
+	])("getDarfs(%p)", (year, expectedDarfs) => {
+		const monthlyProfitLoss = subscriptionTaxCalculator["getMonthlyProfitLoss"](year);
+		expect(
+			subscriptionTaxCalculator["getDarfs"](year, monthlyProfitLoss, SubscriptionTaxCalculator["DARF_RATE"]),
+		).toStrictEqual(expectedDarfs);
+	});
+
+	test.each([2019, 2020, 2021, 2022, 2023, 2024])("getTaxReport(%p)", (year) => {
+		const getSituationReportSpy = jest.spyOn(subscriptionTaxCalculator as any, "getSituationReport");
+		const getMonthlyProfitLossSpy = jest.spyOn(subscriptionTaxCalculator as any, "getMonthlyProfitLoss");
+		const getDarfsSpy = jest.spyOn(subscriptionTaxCalculator as any, "getDarfs");
+
+		subscriptionTaxCalculator.getTaxReport(year);
+
+		expect(getSituationReportSpy).toHaveBeenCalledWith(year);
+		expect(getSituationReportSpy).toHaveBeenCalledTimes(1);
+		expect(getMonthlyProfitLossSpy).toHaveBeenCalledWith(year);
+		expect(getMonthlyProfitLossSpy).toHaveBeenCalledTimes(1);
+		expect(getDarfsSpy).toHaveBeenCalledWith(
+			year,
+			subscriptionTaxCalculator["getMonthlyProfitLoss"](year),
+			SubscriptionTaxCalculator["DARF_RATE"],
+		);
+		expect(getDarfsSpy).toHaveBeenCalledTimes(1);
 	});
 });
