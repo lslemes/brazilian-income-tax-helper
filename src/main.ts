40ac50120c103bcf29c5a6ea4f4fb202e7412ccd
Leonardo Lemes <leonardo.slemes@gmail.com>
2024-04-08 21:14:23 -0300

    chore: sample main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import csv from "csvtojson";
-import EtfTaxCalculator from "./taxCalculators/etfTaxCalculator/EtfTaxCalculator";
-import FiiTaxCalculator from "./taxCalculators/fiiTaxCalculator/FiiTaxCalculator";
-import StockTaxCalculator from "./taxCalculators/stockTaxCalculator/StockTaxCalculator";
-import SubscriptionTaxCalculator from "./taxCalculators/subscriptionTaxCalculator/SubscriptionTaxCalculator";
+import BrazilianEtfTaxCalculator from "./taxCalculators/brazilianEtfTaxCalculator/BrazilianEtfTaxCalculator";
+import BrazilianFiiTaxCalculator from "./taxCalculators/brazilianFiiTaxCalculator/BrazilianFiiTaxCalculator";
+import BrazilianStockTaxCalculator from "./taxCalculators/brazilianStockTaxCalculator/BrazilianStockTaxCalculator";
+import BrazilianSubscriptionTaxCalculator from "./taxCalculators/brazilianSubscriptionTaxCalculator/BrazilianSubscriptionTaxCalculator";
 import Transaction from "./transaction/Transaction";
 import mapCsvTransactionToTransaction, { CsvTransaction } from "./transaction/mapCsvTransactionToTransaction";
 
 async function main() {
-	const csvTransactions: CsvTransaction[] = await csv().fromFile("data/transactions.csv");
+	const csvTransactions: CsvTransaction[] = await csv().fromFile("data/brazilianTransactions.csv");
 	const transactions: Transaction[] = csvTransactions.map(mapCsvTransactionToTransaction);
 
-	const fiiTaxCalculator = new FiiTaxCalculator(transactions);
-	const stockTaxCalculator = new StockTaxCalculator(transactions);
-	const etfTaxCalculator = new EtfTaxCalculator(transactions);
-	const subscriptionTaxCalculator = new SubscriptionTaxCalculator(transactions);
-	// console.log(stockTaxCalculator.getTaxReport(2024));
+	const brazilianFiiTaxCalculator = new BrazilianFiiTaxCalculator(transactions);
+	const brazilianStockTaxCalculator = new BrazilianStockTaxCalculator(transactions);
+	const brazilianEtfTaxCalculator = new BrazilianEtfTaxCalculator(transactions);
+	const brazilianSubscriptionTaxCalculator = new BrazilianSubscriptionTaxCalculator(transactions);
 }
 main();
